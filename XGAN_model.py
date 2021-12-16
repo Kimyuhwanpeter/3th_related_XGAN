bea61c7ae7de40a413a6ab3eb06264d756744564
YYu HHwan KKim <31001511+Kimyuhwanpeter@users.noreply.github.com>
2021-12-16 17:31:23 +0900

    Add files via upload

---
+++ XGAN_model.py
@@ -0,0 +1,172 @@
+# -*- coding:utf-8 -*-
+from tensorflow.python.framework import ops
+
+import tensorflow as tf
+
+class FlipGradientBuilder(object):
+    def __init__(self):
+        self.num_calls = 0
+
+    def __call__(self, x, l=1.0):
+        grad_name = "FlipGradient%d" % self.num_calls
+        @ops.RegisterGradient(grad_name)
+        def _flip_gradients(op, grad):
+            return [tf.negative(grad) * l]
+        
+        g = tf.compat.v1.get_default_graph()
+        with g.gradient_override_map({"Identity": grad_name}):
+            y = tf.identity(x)
+            
+        self.num_calls += 1
+        return y
+    
+flip_gradient = FlipGradientBuilder()
+
+class InstanceNormalization(tf.keras.layers.Layer):
+  #"""Instance Normalization Layer (https://arxiv.org/abs/1607.08022)."""
+
+    def __init__(self, epsilon=1e-5):
+        super(InstanceNormalization, self).__init__()
+        self.epsilon = epsilon
+    
+    def build(self, input_shape):
+        self.scale = self.add_weight(
+            name='scale',
+            shape=input_shape[-1:],
+            initializer=tf.random_normal_initializer(0., 0.02),
+            trainable=True)
+        self.offset = self.add_weight(
+            name='offset',
+            shape=input_shape[-1:],
+            initializer='zeros',
+            trainable=True)
+    
+    def call(self, x):
+        mean, variance = tf.nn.moments(x, axes=[1, 2], keepdims=True)
+        inv = tf.math.rsqrt(variance + self.epsilon)
+        normalized = (x - mean) * inv
+        return self.scale * normalized + self.offset
+
+def conv2d(x, filters, kernel_size=4, strides=2, padding="same"):
+    return tf.keras.layers.Conv2D(filters=filters, kernel_size=kernel_size, strides=strides,
+                                  padding=padding)(x)
+
+def deconv2d(x, filters, kernel_size=4, strides=2):
+    return tf.keras.layers.Conv2DTranspose(filters=filters, kernel_size=kernel_size, strides=strides,
+                                           padding="same")(x)
+
+def encoder(x, filters=32):
+
+    ################## encoder ##################
+    h = InstanceNormalization()(conv2d(x, filters=filters))
+    h = tf.keras.layers.LeakyReLU()(h)
+    h = InstanceNormalization()(conv2d(h, filters=filters*2))
+    h = tf.keras.layers.LeakyReLU()(h)
+    #############################################
+
+    ################## encoder_sharing ##################
+    h = InstanceNormalization()(conv2d(h, filters=filters*4))
+    h = tf.keras.layers.LeakyReLU()(h)
+    h = InstanceNormalization()(conv2d(h, filters=filters*8))
+    h = tf.keras.layers.LeakyReLU()(h)
+    h = conv2d(h, filters=1024, kernel_size=4, strides=1, padding="valid")
+    h = tf.keras.layers.LeakyReLU()(h)
+    h = conv2d(h, filters=1024, kernel_size=1, strides=1, padding="valid")
+    h = tf.keras.layers.LeakyReLU()(h)
+    #####################################################
+    
+    return h
+
+def decoder(x, filters=64):
+
+    h = InstanceNormalization()(deconv2d(x, filters=filters*8, strides=4))
+    h = tf.keras.layers.ReLU()(h)
+    h = InstanceNormalization()(deconv2d(h, filters=filters*4))
+    h = tf.keras.layers.ReLU()(h)
+
+    h = InstanceNormalization()(deconv2d(h, filters=filters*2))
+    h = tf.keras.layers.ReLU()(h)
+    h = InstanceNormalization()(deconv2d(h, filters=filters))
+    h = tf.keras.layers.ReLU()(h)
+    h = deconv2d(h, filters=3)
+    h = tf.nn.tanh(h)
+
+    return h
+
+def cdann(input):
+    
+    fg = flip_gradient(input)
+    c1 = conv2d(fg, filters=1024, kernel_size=1, strides=1, padding="valid")
+    c1 = tf.keras.layers.LeakyReLU()(c1)
+    # c1 is (batch_size x 1 x 1 x 1024)
+    c2 = conv2d(fg, filters=1024, kernel_size=1, strides=1, padding="valid")
+    c2 = tf.keras.layers.LeakyReLU()(c2)
+    # c2 is (batch_size x 1 x 1 x 1024)
+    c3 = conv2d(fg, filters=1024, kernel_size=1, strides=1, padding="valid")
+    c3 = tf.keras.layers.LeakyReLU()(c3)
+    # c3 is (batch_size x 1 x 1 x 1024)
+    c4 = conv2d(fg, filters=1, kernel_size=1, strides=1, padding="valid")
+    # c4 is (batch_size x 1 x 1 x 1)
+
+    return c4
+
+def XGAN(input_shape=(64, 64, 3)):  # 모델을 독립적으로 만들어야함
+
+    A = A_inputs = tf.keras.Input(input_shape)
+    B = B_inputs = tf.keras.Input(input_shape)
+
+    embedding_A = encoder(A)
+    embedding_B = encoder(B)
+
+    # A->encoderA->decoderA
+    # B->encoderB->decoderB
+    reconstruct_A = decoder(embedding_A)
+    reconstruct_B = decoder(embedding_B)
+
+    # Cdann output
+    cdann_A = cdann(embedding_A)
+    cdann_B = cdann(embedding_B)
+
+    # Generator output
+    # B->encoderB->decoderA
+    # A->encoderA->decoderB
+    fake_A = decoder(embedding_B)
+    fake_B = decoder(embedding_A)
+
+    embedding_fake_A = encoder(fake_A)
+    embedding_fake_B = encoder(fake_B)
+
+
+    return tf.keras.Model(inputs=[A_inputs, B_inputs], outputs=[reconstruct_A,
+                                                                reconstruct_B,
+                                                                cdann_A,
+                                                                cdann_B,
+                                                                embedding_A,
+                                                                embedding_B,
+                                                                embedding_fake_A,
+                                                                embedding_fake_B,
+                                                                fake_A,
+                                                                fake_B])
+
+def discriminator(input_shape=(64, 64, 3), filters=16):
+
+    h = inputs = tf.keras.Input(input_shape)
+
+    h = conv2d(h, filters=filters)
+    h = tf.keras.layers.LeakyReLU()(h)
+
+    h = conv2d(h, filters=filters*2)
+    h = InstanceNormalization()(h)
+    h = tf.keras.layers.LeakyReLU()(h)
+
+    h = conv2d(h, filters=filters*2)
+    h = InstanceNormalization()(h)
+    h = tf.keras.layers.LeakyReLU()(h)
+
+    h = conv2d(h, filters=filters*2)
+    h = InstanceNormalization()(h)
+    h = tf.keras.layers.LeakyReLU()(h)
+
+    h = conv2d(h, filters=1, kernel_size=4, strides=1, padding="valid")
+
+    return tf.keras.Model(inputs=inputs, outputs=h)
---
+++ XGAN_train.py
@@ -0,0 +1,202 @@
+# -*- coding:utf-8 -*-
+from random import shuffle
+from XGAN_model import *
+
+import tensorflow as tf
+import matplotlib.pyplot as plt
+import numpy as np
+import easydict
+
+FLAGS = easydict.EasyDict({"img_size": 64,
+                           
+                           "batch_size": 16,
+                           
+                           "lr": 0.0001,
+
+                           "epochs": 100,
+                           
+                           "A_txt_path": "D:/[1]DB/[2]third_paper_DB/[4]Age_and_gender/race_age_gender_generation/Morph_AFAD_16_63/first_fold/AFAD-F_Morph-M_16_39_40_63/train/female_16_39_train.txt",
+                           
+                           "A_img_path": "D:/[1]DB/[1]second_paper_DB/AFAD_16_69_DB/backup/fix_AFAD/",
+                           
+                           "B_txt_path": "D:/[1]DB/[2]third_paper_DB/[4]Age_and_gender/race_age_gender_generation/Morph_AFAD_16_63/first_fold/AFAD-F_Morph-M_16_39_40_63/train/male_40_63_train.txt",
+                           
+                           "B_img_path": "D:/[1]DB/[2]third_paper_DB/[4]Age_and_gender/Morph/All/male_40_63/",
+                           
+                           "pre_checkpoint": False,
+                           
+                           "pre_checkpoint_path": "",
+                           
+                           "save_checkpoint": "",
+                           
+                           "sample_images": "C:/Users/Yuhwan/Downloads/dd"})
+
+g_optim = tf.keras.optimizers.Adam(FLAGS.lr, beta_1=0.5)
+d_optim = tf.keras.optimizers.Adam(FLAGS.lr, beta_1=0.5)
+
+def _func(A_filename, B_filename):
+
+    h = tf.random.uniform([1], 1e-2, 5)
+    h = tf.cast(tf.math.ceil(h[0]), tf.int32)
+    w = tf.random.uniform([1], 1e-2, 5)
+    w = tf.cast(tf.math.ceil(w[0]), tf.int32)
+    
+    A_image_string = tf.io.read_file(A_filename)
+    A_decode_image = tf.image.decode_jpeg(A_image_string, channels=3)
+    A_decode_image = tf.image.resize(A_decode_image, [FLAGS.img_size + 5, FLAGS.img_size + 5])
+    A_decode_image = A_decode_image[h:h+FLAGS.img_size, w:w+FLAGS.img_size, :]
+    A_decode_image = tf.image.convert_image_dtype(A_decode_image, tf.float32) / 127.5 - 1.
+
+    B_image_string = tf.io.read_file(B_filename)
+    B_decode_image = tf.image.decode_jpeg(B_image_string, channels=3)
+    B_decode_image = tf.image.resize(B_decode_image, [FLAGS.img_size + 5, FLAGS.img_size + 5])
+    B_decode_image = B_decode_image[h:h+FLAGS.img_size, w:w+FLAGS.img_size, :]
+    B_decode_image = tf.image.convert_image_dtype(B_decode_image, tf.float32) / 127.5 - 1.
+
+    if tf.random.uniform(()) > 0.5:
+        A_decode_image = tf.image.flip_left_right(A_decode_image)
+        B_decode_image = tf.image.flip_left_right(B_decode_image)
+
+    return A_decode_image, B_decode_image
+
+def euc_criterion(in_, target):
+    return tf.keras.losses.MeanSquaredError()(in_, target)
+def abs_criterion(in_, target):
+    return tf.reduce_mean(tf.abs(in_ - target))
+def mae_criterion(in_, target):
+    return tf.reduce_mean((in_ - target)**2)
+def sce_criterion(logits, labels):
+    return tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=logits, labels=labels))
+
+def cal_loss(g_model, A_de_model, B_de_model, A_images, B_images):
+
+    with tf.GradientTape(persistent=True) as g_tape, tf.GradientTape(persistent=True) as d_tape:
+
+        reconstruct_A, \
+        reconstruct_B, \
+        cdann_A, \
+        cdann_B, \
+        embedding_A, \
+        embedding_B, \
+        embedding_fake_A, \
+        embedding_fake_B, \
+        fake_A, \
+        fake_B = g_model([A_images, B_images], True)
+
+        # Discrim output
+        discriminate_real_A = A_de_model(A_images, True)
+        discriminate_real_B = B_de_model(B_images, True)
+        discriminate_fake_A = A_de_model(fake_A, True)
+        discriminate_fake_B = B_de_model(fake_B, True)
+
+        # Recon loss
+        rec_loss_A = euc_criterion(A_images, reconstruct_A)
+        rec_loss_B = euc_criterion(B_images, reconstruct_B)
+        rec_loss = rec_loss_A + rec_loss_B
+
+        # domain-advar loss
+        dann_loss = sce_criterion(cdann_A, tf.zeros_like(cdann_A)) + sce_criterion(cdann_B, tf.ones_like(cdann_B))
+
+        # semantic consistency loss
+        sem_loss_A = abs_criterion(embedding_A, embedding_fake_B)
+        sem_loss_B = abs_criterion(embedding_B, embedding_fake_A)
+        sem_loss = sem_loss_A + sem_loss_B
+
+        # GAN loss-generator
+        gen_gan_loss_A = mae_criterion(discriminate_fake_A, tf.ones_like(discriminate_fake_A))
+        gen_gan_loss_B = mae_criterion(discriminate_fake_B, tf.ones_like(discriminate_fake_B))
+        gen_gan_loss = gen_gan_loss_A + gen_gan_loss_B
+
+        # Total loss
+        gen_loss = rec_loss + dann_loss + sem_loss + gen_gan_loss
+
+        # Gan loss-discriminator
+        dis_gan_loss_real_A = mae_criterion(discriminate_real_A, tf.ones_like(discriminate_real_A))
+        dis_gan_loss_fake_A = mae_criterion(discriminate_fake_A, tf.zeros_like(discriminate_fake_A))
+        dis_gan_loss_A = (dis_gan_loss_real_A + dis_gan_loss_fake_A) / 2
+        dis_gan_loss_real_B = mae_criterion(discriminate_real_B, tf.ones_like(discriminate_real_B))
+        dis_gan_loss_fake_B = mae_criterion(discriminate_fake_B, tf.zeros_like(discriminate_fake_B))
+        dis_gan_loss_B = (dis_gan_loss_real_B + dis_gan_loss_fake_B) / 2
+        dis_gan_loss = dis_gan_loss_A + dis_gan_loss_B
+
+        # Total loss
+        dis_loss = dis_gan_loss
+
+    g_grads = g_tape.gradient(gen_loss, g_model.trainable_variables)
+    d_grads = d_tape.gradient(dis_loss, A_de_model.trainable_variables + B_de_model.trainable_variables)
+
+    g_optim.apply_gradients(zip(g_grads, g_model.trainable_variables))
+    d_optim.apply_gradients(zip(d_grads, A_de_model.trainable_variables + B_de_model.trainable_variables))
+
+
+    return gen_loss, dis_loss
+
+def main():
+
+    ge_model = XGAN(input_shape=(FLAGS.img_size, FLAGS.img_size, 3))
+    A_de_model = discriminator(input_shape=(FLAGS.img_size, FLAGS.img_size, 3))
+    B_de_model = discriminator(input_shape=(FLAGS.img_size, FLAGS.img_size, 3))
+    ge_model.summary()
+    A_de_model.summary()
+    B_de_model.summary()
+
+    if FLAGS.pre_checkpoint:
+        ckpt = tf.train.Checkpoint(ge_model=ge_model, A_de_model=A_de_model, B_de_model=B_de_model,
+                                   g_optim=g_optim, d_optim=d_optim)
+        ckpt_manager = tf.train.CheckpointManager(ckpt, FLAGS.pre_checkpoint_path, 5)
+
+        if ckpt_manager.latest_checkpoint:
+            ckpt.restore(ckpt_manager.latest_checkpoint)
+
+    A_input = np.loadtxt(FLAGS.A_txt_path, dtype="<U200", skiprows=0, usecols=0)
+    A_input = [FLAGS.A_img_path + data for data in A_input]
+
+    B_input = np.loadtxt(FLAGS.B_txt_path, dtype="<U200", skiprows=0, usecols=0)
+    B_input = [FLAGS.B_img_path + data for data in B_input]
+
+    count = 0
+    for epoch in range(FLAGS.epochs):
+
+        shuffle(A_input)
+        shuffle(B_input)
+
+        A_input, B_input = np.array(A_input), np.array(B_input)
+        
+        tr_gener = tf.data.Dataset.from_tensor_slices((A_input, B_input))
+        tr_gener = tr_gener.shuffle(len(A_input))
+        tr_gener = tr_gener.map(_func)
+        tr_gener = tr_gener.batch(FLAGS.batch_size)
+        tr_gener = tr_gener.prefetch(tf.data.experimental.AUTOTUNE)
+        
+        tr_iter = iter(tr_gener)
+        tr_idx = len(A_input) // FLAGS.batch_size
+        for step in range(tr_idx):
+            A_images, B_images = next(tr_iter)
+            a = 0
+
+            g_loss, d_loss = cal_loss(ge_model, A_de_model, B_de_model, A_images, B_images)
+
+            if count % 100 == 0:
+                print("Epochs: {} g_loss = {} d_loss = {} [{}/{}]".format(epoch, g_loss, d_loss, step + 1, tr_idx))
+
+                _, _, _, _, _, _, _, _, fake_A, fake_B = ge_model([A_images, B_images], False)
+                plt.imsave(FLAGS.sample_images + "/" + "{}_1_A_real.png".format(count), A_images[0].numpy() * 0.5 + 0.5)
+                plt.imsave(FLAGS.sample_images + "/" + "{}_2_A_real.png".format(count), A_images[1].numpy() * 0.5 + 0.5)
+                plt.imsave(FLAGS.sample_images + "/" + "{}_3_A_real.png".format(count), A_images[2].numpy() * 0.5 + 0.5)
+                plt.imsave(FLAGS.sample_images + "/" + "{}_1_B_real.png".format(count), B_images[0].numpy() * 0.5 + 0.5)
+                plt.imsave(FLAGS.sample_images + "/" + "{}_2_B_real.png".format(count), B_images[1].numpy() * 0.5 + 0.5)
+                plt.imsave(FLAGS.sample_images + "/" + "{}_3_B_real.png".format(count), B_images[2].numpy() * 0.5 + 0.5)
+
+                plt.imsave(FLAGS.sample_images + "/" + "{}_1_A_fake.png".format(count), fake_A[0].numpy() * 0.5 + 0.5)
+                plt.imsave(FLAGS.sample_images + "/" + "{}_2_A_fake.png".format(count), fake_A[1].numpy() * 0.5 + 0.5)
+                plt.imsave(FLAGS.sample_images + "/" + "{}_3_A_fake.png".format(count), fake_A[2].numpy() * 0.5 + 0.5)
+                plt.imsave(FLAGS.sample_images + "/" + "{}_1_B_fake.png".format(count), fake_B[0].numpy() * 0.5 + 0.5)
+                plt.imsave(FLAGS.sample_images + "/" + "{}_2_B_fake.png".format(count), fake_B[1].numpy() * 0.5 + 0.5)
+                plt.imsave(FLAGS.sample_images + "/" + "{}_3_B_fake.png".format(count), fake_B[2].numpy() * 0.5 + 0.5)
+
+
+
+            count += 1
+
+if __name__ == "__main__":
+    main()
